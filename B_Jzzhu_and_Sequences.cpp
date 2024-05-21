6e53bbf5d27d7a86b9e86cbffb7909e84404d109
Raj Singh <rajsingh@kgpian.iitkgp.ac.in>
2024-05-21 20:33:35 +0530

    pattern after 6 turns

---
+++ B_Jzzhu_and_Sequences.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define ul unsigned long long 
+#define ld long double 
+#define fl(i,start,end) for(ll i=start;i<end;i++)
+#define M 1000000007
+#define endl '\n' 
+using namespace std; 
+
+void solve() {
+    ll x,y, n; cin >> x >> y >> n;
+    int a[6] = {x,y, y-x, -1*x, -1*y, -1*y + x};
+    n--;
+    cout << (a[n%6] + M)%M;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    // int _t; cin >> _t; while(_t--) 
+        solve();
+    return 0;
+}
