4cd061a6b628b5efde3afbc3e01753d8af7c117d
Raj Singh <rajsingh@kgpian.iitkgp.ac.in>
2024-05-28 17:56:51 +0530

    pattern - for odd n print any odd n times for even print 1 3 and 2 n-2 times

---
+++ B_XOR_Average.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define ul unsigned long long 
+#define ld long double 
+#define fl(i,start,end) for(ll i=start;i<end;i++)
+#define M 1000000007
+#define endl '\n' 
+using namespace std; 
+
+void solve() {
+    int n; cin >> n;
+    if(n&1) while(n--) cout << "69 ";
+    else {
+        cout << "1 3 ";
+        n -= 2;
+        while(n--) cout << "2 ";
+    }
+    cout << endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int _t; cin >> _t; while(_t--) 
+        solve();
+    return 0;
+}
