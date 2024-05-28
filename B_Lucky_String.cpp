1e83bf96576a9c2d42f7201bf0a7e734344fb7ab
Raj Singh <rajsingh@kgpian.iitkgp.ac.in>
2024-05-28 12:59:58 +0530

    abcd repetition

---
+++ B_Lucky_String.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define ul unsigned long long 
+#define ld long double 
+#define fl(i,start,end) for(ll i=start;i<end;i++)
+#define M 1000000007
+#define endl '\n' 
+using namespace std; 
+
+void solve() {
+    int n; cin >> n;
+    int c = n/4;
+    while(c--) cout << "abcd";
+    n %= 4, c= 0;
+    while(n--) cout << char('a' + c++);
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    // int _t; cin >> _t; while(_t--) 
+        solve();
+    return 0;
+}
